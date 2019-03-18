a331d1cf97138b561b876f297ecbfb9081025081
Ubuntu <ubuntu@ip-10-100-1-237.us-west-2.compute.internal>
2019-03-18 08:27:54 +0000

    updated h2oinstaller

--- h2oinstaller.py
+++ h2oinstaller.py
@@ -3,10 +3,6 @@ import argparse
 import os
 import time
 import sys
-import yaml
-
-with open("config.yaml", 'r') as ymlfile:
-    cfg = yaml.load(ymlfile)
 
 def prerequisites():
     print ("installing pre requisites")
@@ -15,7 +11,7 @@ def prerequisites():
 def run():
     print ("starting h2o")
     import h2o
-    h2o.init(ip=cfg["h2o"]["ip"], port=cfg["h2o"]["port"] )
+    h2o.init()
     print ("H2o session is running. Press Ctrl + Z to run the H2o process in background.")
     while True:
         time.sleep(20)
@@ -29,7 +25,6 @@ def install():
         os.system("pip install -f http://h2o-release.s3.amazonaws.com/h2o/latest_stable_Py.html h2o --user")
     else:
         os.system("pip install -f http://h2o-release.s3.amazonaws.com/h2o/latest_stable_Py.html h2o")
-    run()
 
 def uninstall():
     os.system("pip uninstall h2o -y")
